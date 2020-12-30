d9158648d71ada82f4234c9e0167905a10f86736
Mulumba Uchenna Ede <56554779+Mulumba1@users.noreply.github.com>
2020-12-30 14:32:08 +0100

    Add files via upload

---
+++ wordGues Game.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+namespace DevUniguessgame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "mulumba";
+            string guess =  "";
+            int guessCount = 0;
+            int guesLimit = 5;
+            bool outOfGuess = false;
+
+            while(guess != secretWord && !outOfGuess)
+            {
+                if(guessCount < guesLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount ++; 
+                    
+                }
+                else
+                {
+                    outOfGuess = true;
+                }
+            }
+            if (outOfGuess)
+                {
+                    Console.Write("Oh sorry! You Loss");
+                } 
+            else
+            {
+                Console.Write("Congratulations! You win");
+        
+            }
+        }           
+    }
+}
